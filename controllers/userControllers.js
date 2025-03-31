88c36f47f5ed83467c6aed262c8493508287349f
Sarthak <shindesarthak10@gmail.com>
2025-03-31 17:10:10 +0530

    test

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -22,6 +22,7 @@ exports.test = async (req, res) => {
     return res.status(200).json({
       message: "success",
     });
+    
   } catch (error) {
     console.log(error);
     return res.status(500).json({
